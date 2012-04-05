ef3e1a8f3e135204363d2d0b1cbdb1261c0e2cd9
Maksym Madylyus <mmadyl@softserveinc.com>
2012-04-05 19:11:35 +0300

    Comment
    Signed-off-by: Maksym Madylyus <mmadyl@softserveinc.com>

---
+++ src/Domain/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain
+{
+    public class Test
+    {
+        
+        #region private fields
+
+        private string _taskText = String.Empty;
+
+        #endregion
+
+        #region public properties
+
+        /// <summary>
+        /// Gets or sets task text
+        /// </summary>
+        public string TaskText
+        {
+            get { return _taskText; }
+            set { _taskText = value; }
+        }
+
+        #endregion
+
+    }
+}
