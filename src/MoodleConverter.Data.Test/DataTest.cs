9e4ad279414bd2fad5764de88aee8b24689e5798
MaksymMadylyus <mmadyl@softserveinc.com>
2012-04-24 15:12:37 +0300

    DBContext for EF added, Data test added
    Signed-off-by: MaksymMadylyus <mmadyl@softserveinc.com>

---
+++ src/MoodleConverter.Data.Test/DataTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MoodleConverter.Data;
+
+namespace MoodleConverter.Data.Test
+{
+    [TestFixture]
+    public class DataTest
+    {
+        [Test]
+        public void CreateDBTest()
+        {
+            var context = new MoodleConverterDBContext();
+            if (!context.Database.Exists())
+            {
+                context.Database.Initialize(false);
+            }
+            Assert.IsTrue(context.Database.Exists());
+        }
+    }
+}
